310ada63eb92ff12ef7c65b519394df9262a2e51
NoxiaLupus <noxia.lupus@gmail.com>
2023-03-27 16:36:00 +0200

    Edit the start page and make styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'start', component: AppComponent, pathMatch: 'full'}
+] 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
